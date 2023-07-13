fc3239ff98eae63080d413463aacb0affd2b619a
khairul-bashar <khairulbashar5424@gmail.com>
2023-07-13 17:38:19 +0600

    Stater Pack install

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const data = require('./database/chef.json')
+const app = express()
+const cors = require('cors')
+const port = process.env.Port || 3000
+
+
+app.use(cors())
+app.get("/", (req, res) => {
+  res.send("Api port is running")
+})
+
+app.get('/data', (req, res) => {
+  res.send(data)
+})
+
+app.get('/data/:id', (req, res) => {
+  const id = req.params.id
+  const sData = data.find(sData => sData._id === id)
+  res.send(sData)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
